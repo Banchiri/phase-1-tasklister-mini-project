0928085f4c82b5cbc21371a35f054080d8267a8a
sandrabanchiri <sandrabanchiri39@gmail.com>
2024-07-23 17:09:35 +0300

    newfile

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.getElementById("create-task-form");
+  let tasklist = document.getElementById("tasks");
+
+  form.addEventListener("submit", function(event){
+    event.preventDefault();
+      
+  
+    const taskDescription = document.getElementById("new-task-description").value;
+    if (taskDescription.trim() !== "") {
+       const taskItem = document.createElement("li");
+       taskItem.textContent = taskDescription;
+       tasklist.appendChild(taskItem);
+
+
+       document.getElementById("new-task-description").value = "";
+   }
+  });
 });
